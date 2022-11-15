c91a95e5789796f704138bbf1d6ed1425862daf5
Antonio Evaristo <antonio.c.e.barbosa@gmail.com>
2022-11-14 22:21:04 -0300

    iniciando projeto

---
+++ produto/src/test/java/pocspringresilience4j/produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package pocspringresilience4j.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
